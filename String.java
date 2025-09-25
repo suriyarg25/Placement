7fa0a3ecbfc20cd7e9dd9d2c05d214f73368492c
Rohit <rohit.al24@krct.ac.in>
2025-09-25 10:07:12 +0530

    rohit

--- String.java
+++ String.java
@@ -1,3 +1,15 @@
-public class String {
-    
+
+import java.util.Scanner;
+
+public class Strings{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String str1 = input.nextLine();
+
+        System.out.println("Hello, "+str1);
+        int strlen = str1.length();
+        System.out.println("The length of your name is: "+strlen);
+
+    }
 }
